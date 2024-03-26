df578639ec2decc7537c56ac623bd57c0c6c80c7
Rish-XO <rishal.mhd009@gmail.com>
2024-03-26 20:57:19 +0530

    save

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import logo from "./logo.svg";
-import "./App.css";
 import React, { useState } from "react";
+import "./App.css";
 
 function App() {
   const [layout, setLayout] = useState(1); // Default layout: 1 image
 
   const images = [
-    "https://images.unsplash.com/photo-1710964932529-bd0b2e48c5ea?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    "https://images.unsplash.com/photo-1710961716370-dfff65e2ba0c?q=80&w=2000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    "https://images.unsplash.com/photo-1710961716370-dfff65e2ba0c?q=80&w=2000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    "https://images.unsplash.com/photo-1711109631427-f90f9d83364b?q=80&w=2535&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1711144035142-42d005a8ad48?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1711345545708-8113c939c63b?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1696406519677-9b8ca4b1cf59?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1711251855398-dade56b02b78?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   ];
 
   // Function to handle layout change
@@ -24,24 +23,24 @@ function App() {
           <img src={images[0]} alt="Image 1" />
         </div>
       );
-    } else if (layout == 2) {
+    } else if (layout === 2) {
       return (
-        <div>
+        <>
           <div className="image-container">
             <img src={images[0]} alt="Image 1" />
           </div>
           <div className="image-container">
             <img src={images[1]} alt="Image 2" />
           </div>
-        </div>
+        </>
       );
-    } else if (layout == 3) {
+    } else if (layout === 3) {
       return images.slice(0, 3).map((imageUrl, index) => (
         <div key={index} className="image-container">
           <img src={imageUrl} alt={`Image ${index + 1}`} />
         </div>
       ));
-    } else if (layout == 4) {
+    } else if (layout === 4) {
       return images.map((imageUrl, index) => (
         <div key={index} className="image-container">
           <img src={imageUrl} alt={`Image ${index + 1}`} />
